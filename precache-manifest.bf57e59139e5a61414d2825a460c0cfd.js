40da07549ff53b0b581473f8622393d92707a0cb
zhangbx <zhangbx@didiglobal.com>
2020-01-21 17:01:49 +0800

    deploy blog

---
+++ precache-manifest.bf57e59139e5a61414d2825a460c0cfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed0858cd3114097cf9889cb376f14a08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20277af168f370c44c43",
+    "url": "/static/css/main.3f59946c.chunk.css"
+  },
+  {
+    "revision": "39a711a779ff66afa751",
+    "url": "/static/js/2.95c8a2ea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.95c8a2ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20277af168f370c44c43",
+    "url": "/static/js/main.4f42cdd5.chunk.js"
+  },
+  {
+    "revision": "313bfc7444da2fec65d4",
+    "url": "/static/js/runtime-main.e5e8f68d.js"
+  }
+]);
